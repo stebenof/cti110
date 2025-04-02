15df60e810736d4d755633e82a34f2d9b91bd409
stebenof <currinm8045@student.faytechcc.edu>
2025-04-01 22:15:02 -0400

    Add files via upload

---
+++ P1LAB1_CurrinMia.py
@@ -0,0 +1,16 @@
+#Mia Currin
+#4/1/25
+#P1LAB1
+#Intro to Python
+
+print('Enter your first name:', end='   ')
+first_name= input()
+print('Enter your last name:', end='   ')
+last_name= input()
+print('Hello,', first_name, last_name,'! Welcome to CTI-110')
+
+
+
+
+
+      
